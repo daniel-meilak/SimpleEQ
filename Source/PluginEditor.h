500554a1513f3919a37c36d21596babcc1b9ed63
daniel.mlak@gmail.com <daniel.mlak@gmail.com>
2022-02-13 00:07:11 +0100

    Refactored peak coefficient generation

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -19,7 +19,9 @@ struct CustomRotarySlider : juce::Slider {
 //==============================================================================
 /**
 */
-class SimpleEQAudioProcessorEditor  : public juce::AudioProcessorEditor
+class SimpleEQAudioProcessorEditor : public juce::AudioProcessorEditor,
+    juce::AudioProcessorParameter::Listener,
+    juce::Timer
 {
 public:
     SimpleEQAudioProcessorEditor (SimpleEQAudioProcessor&);
@@ -29,10 +31,18 @@ public:
     void paint (juce::Graphics&) override;
     void resized() override;
 
+    void parameterValueChanged(int parameterIndex, float newValue) override;
+
+    void parameterGestureChanged(int parameterIndex, bool gestureIsStarting) override { };
+
+    void timerCallback() override;
+
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     SimpleEQAudioProcessor& audioProcessor;
+
+    juce::Atomic<bool> parametersChanged{ false };
 
     CustomRotarySlider peakFreqSlider, peakGainSlider, peakQualitySlider, lowCutFreqSlider, highCutFreqSlider, lowCutSlopeSlider, highCutSlopeSlider;
 
@@ -44,7 +54,7 @@ private:
 
     std::vector<juce::Component*> getComps();
 
-    MonoChain monoChains;
+    MonoChain monoChain;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessorEditor)
 };
